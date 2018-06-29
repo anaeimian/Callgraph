d533b3fd662a31ed59f0eaece44e5604fd0667b5
anaeimia <armannaeimian@gmail.com>
2018-06-29 17:54:10 -0400

    Adding files to repository

--- process_bug_propagation.py
+++ process_bug_propagation.py
@@ -20,19 +20,23 @@ def edge_exists_in_graph(edge_info, src_date, dst_date):
 i = 0
 with open('edge_life_cycle_map.txt', 'r') as edge_life_cycle_map_file:
     edge_life_cycle_map = eval(edge_life_cycle_map_file.read())
-with open('vertex_bug_changes_date_map.txt', 'r') as vertex_bug_changes_file:
+with open('vertex_changes_map_dates_commits.txt', 'r') as vertex_bug_changes_file:
     vertex_bug_changes = eval(vertex_bug_changes_file.read())
 for edge, info in edge_life_cycle_map.items():
     src_vertex = edge[0]
     dst_vertex = edge[1]
     edge_dates = info['dates']
-    src_date_array = vertex_bug_changes[src_vertex]
-    dst_date_array = vertex_bug_changes[dst_vertex]
+    src_date_array = vertex_bug_changes[src_vertex]['dates']
+    dst_date_array = vertex_bug_changes[dst_vertex]['dates']
     if len(src_date_array) > 0 and len(dst_date_array) > 0:
         src_date_min = src_date_array[0]
         dst_date_min = dst_date_array[0]
+        src_commit = vertex_bug_changes[src_vertex]['commits'][0]
+        dst_commit = vertex_bug_changes[dst_vertex]['commits'][0]
         if edge_exists_in_graph(info, int(src_date_min), int(dst_date_min)):
             if src_date_min < dst_date_min:
+                print(edge)
                 i += 1
+        print(src_commit.strip(), dst_commit.strip())
 
 print(i)
